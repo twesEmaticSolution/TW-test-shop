427bab8402106a9a9633b0ad1191d454430d824b
twesEmaticSolution <104129000+twesEmaticSolution@users.noreply.github.com>
2023-12-25 12:30:58 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ import OrderScreen from "./screens/OrderScreen";
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Router from 'react-router'
-import routes from './routes'
+import routes from 'routes'
  
 import TagManager from 'react-gtm-module'
  
